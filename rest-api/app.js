7775a4fb714866cd19839394a928026c8788603e
Ederson Torresini <etorresini@ifsc.edu.br>
2026-07-30 18:23:58 -0300

    Adicionar código de exemplo de Docker Compose

---
+++ rest-api/app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello, World!" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
